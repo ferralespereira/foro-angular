1a9ec4393c83d184d85cf42e6084612d3445ff42
javier ferrales <ferralespereira@gmail.com>
2021-08-03 18:10:39 -0400

    add home component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,12 +3,14 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // Importar componentes
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 // Array de tutas
 const appRoutes: Routes = [
-  { path: '', component:LoginComponent },
+  { path: '', component:HomeComponent },
+  { path: 'home', component:HomeComponent },
   { path: 'login', component:LoginComponent },
   { path: 'register', component:RegisterComponent },
   { path: '**', component:LoginComponent }
