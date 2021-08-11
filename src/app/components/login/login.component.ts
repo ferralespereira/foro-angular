b16e3b87a043755c7e3b0f30f447472faa4a2c36
javier ferrales <ferralespereira@gmail.com>
2021-08-11 16:28:07 -0400

    saving identity and token in localStorage

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
 
@@ -17,7 +18,9 @@ export class LoginComponent implements OnInit {
   public token: any;
 
   constructor(
-    private _userService:UserService
+    private _userService:UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
   ){
     this.page_title = "Login";
     this.user = new User('','','','','','','ROLE_USER');
@@ -35,6 +38,7 @@ export class LoginComponent implements OnInit {
         if(response.user && response.user._id){
           // guardamos el usuario en una propiedad
           this.identity = response;
+          localStorage.setItem('identity', JSON.stringify(this.identity));
 
           // conseguir el token del usuario identificado
           this._userService.signup(this.user, this.gettoken).subscribe(
@@ -42,6 +46,10 @@ export class LoginComponent implements OnInit {
               if(response.token){
                 // guardamos el token del usuario
                 this.token = response.token;
+                localStorage.setItem('token', this.token);
+
+                this.status = 'success';
+                this._router.navigate(['home']);
               }else{
                 this.status = 'error';
               }
