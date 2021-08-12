1567e6ab9b7bd0f3e5e9cd5c89a5dc065f6e469a
javier ferrales <ferralespereira@gmail.com>
2021-08-12 17:56:58 -0400

    component user-edit1

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+  styleUrls: ['./user-edit.component.css'],
+  providers: [UserService]
 })
 export class UserEditComponent implements OnInit {
+  public page_title: string;
+  public user: User;
+  public identity: any;
+  public token: any;
+  public status: string;
 
-  constructor() { }
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _userService: UserService
+  ){
+    this.page_title = 'User settings';
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.user = this.identity;
+    this.status = 'error';
+  }
 
   ngOnInit(): void {
   }
