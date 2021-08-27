f0041b9dba0d6f55b435ab13951934d5bddd7f4e
javier ferrales <ferralespereira@gmail.com>
2021-08-27 17:30:04 -0400

    showing list

--- src/app/panel/components/list/list.component.ts
+++ src/app/panel/components/list/list.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { Topic } from '../../../models/topic';
+import { UserService } from '../../../services/user.service';
+import { TopicService } from '../../../services/topic.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  providers: [UserService, TopicService]
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  public page_title: string;
+  // public topics: Array<Topic>;
+  public identity: any;
+  public token: any;
+  public status: any;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _userService:UserService,
+    private _topicService:TopicService
+  ){
+    this.page_title = "Topic List";
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+  }
 
   ngOnInit(): void {
   }
