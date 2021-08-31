c67fe783da5beeafb5230eb870ad78d7c93c94c8
javier ferrales <ferralespereira@gmail.com>
2021-08-31 17:58:05 -0400

    showing topics

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { UserEditComponent } from './components/user-edit/user-edit.component';
+import { TopicsComponent } from './components/topics/topics.component';
 
 // Array de tutas
 const appRoutes: Routes = [
@@ -15,6 +16,8 @@ const appRoutes: Routes = [
   { path: 'login', component:LoginComponent },
   { path: 'register', component:RegisterComponent },
   { path: 'settings', component:UserEditComponent },
+  { path: 'topics', component:TopicsComponent },
+  { path: 'topics/:page', component:TopicsComponent },
   { path: '**', component:LoginComponent }
 ];
 
