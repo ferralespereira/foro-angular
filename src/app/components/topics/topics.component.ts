cb906884ff06ffb80f0f86c31f3bb29e8d98f257
javier ferrales <ferralespereira@gmail.com>
2021-09-01 18:44:38 -0400

    st2 (paginate)

--- src/app/components/topics/topics.component.ts
+++ src/app/components/topics/topics.component.ts
@@ -17,6 +17,7 @@ export class TopicsComponent implements OnInit {
   public page: any;
   public next_page: any;
   public prev_page: any;
+  public number_pages:any;
 
   constructor(
     private _route: ActivatedRoute,
@@ -28,17 +29,47 @@ export class TopicsComponent implements OnInit {
 
 
   ngOnInit(): void {
-    this.getTopics(1);
+
+    this._route.params.subscribe(params => {
+      var page = parseInt(params['page']);
+
+      if(!page){
+        page = 1;
+        this.prev_page = 1;
+        this.next_page = 2;
+      }
+      this.getTopics(page);
+    });
   }
 
   getTopics(page = 1){
     this._topicService.getTopics(page).subscribe(
       response => {
         if(response.topics){
+
           this.topics = response.topics;
 
           // navegacion de paginacion
+          this.totalPages = response.totalPages;
 
+          var number_pages = [];
+          for(var i = 1;  i <= this.totalPages; i++){
+            number_pages.push(i);
+          }
+
+          this.number_pages = number_pages;
+
+          if(page >= 2){
+            this.prev_page = page-1;
+          }else{
+            this.prev_page = 1;
+          }
+
+          if(page < this.totalPages){
+            this.next_page = page + 1;
+          }else{
+            this.next_page = this.totalPages;
+          }
 
         }else{
           this._router.navigate(['/home']);
