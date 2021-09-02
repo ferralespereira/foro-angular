14fcb3cdc66a7bb60dc531cca2f5e823b5c6d3be
javier ferrales <ferralespereira@gmail.com>
2021-09-02 17:36:55 -0400

    showing topic detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { UserEditComponent } from './components/user-edit/user-edit.component';
 import { TopicsComponent } from './components/topics/topics.component';
+import { TopicDetailComponent } from './components/topic-detail/topic-detail.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { TopicsComponent } from './components/topics/topics.component';
     LoginComponent,
     HomeComponent,
     UserEditComponent,
-    TopicsComponent
+    TopicsComponent,
+    TopicDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { UserEditComponent } from './components/user-edit/user-edit.component';
 import { TopicsComponent } from './components/topics/topics.component';
+import { TopicDetailComponent } from './components/topic-detail/topic-detail.component';
 
 // Array de tutas
 const appRoutes: Routes = [
@@ -18,7 +19,8 @@ const appRoutes: Routes = [
   { path: 'settings', component:UserEditComponent },
   { path: 'topics', component:TopicsComponent },
   { path: 'topics/:page', component:TopicsComponent },
-  { path: '**', component:LoginComponent }
+  { path: 'topic/:id', component:TopicDetailComponent },
+  { path: '**', component:HomeComponent }
 ];
 
 // Exportar configuracion
