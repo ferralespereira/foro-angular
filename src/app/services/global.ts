96835c5e6c60cbf84209fbb97da59304f95f00a7
javier ferrales <ferralespereira@gmail.com>
2021-10-08 18:49:55 -0400

    detail

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,5 @@
 export var global = {
 //  url: 'http://localhost:3999/api/'
-   url: 'http://13.59.91.199:3999/api/'
+   // url: 'https://13.59.91.199:3999/api/'
+   url: 'https://test.javierfolder.com:3999/api/'
 };
